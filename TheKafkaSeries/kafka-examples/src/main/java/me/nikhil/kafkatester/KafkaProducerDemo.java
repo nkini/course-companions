79d8c9398478cc04ca497883eadfcba5751102d5
Nikhil Kini <nkini@ucsc.edu>
2017-10-03 23:11:43 -0700

    Created the producer in the likeness of the consumer so that they both work with command line arguments and properties files now.

--- TheKafkaSeries/kafka-examples/src/main/java/me/nikhil/kafkatester/KafkaProducerDemo.java
+++ TheKafkaSeries/kafka-examples/src/main/java/me/nikhil/kafkatester/KafkaProducerDemo.java
@@ -1,59 +1,28 @@
 package me.nikhil.kafkatester;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Properties;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
 
 public class KafkaProducerDemo {
 
 	public static void main(String args[]) {
 
-		Properties prop = new Properties();
-		InputStream input = null;
-
-		try {
-			String filename = "producer.properties";
-			input = KafkaProducerDemo.class.getClassLoader().getResourceAsStream(filename);
-			if (input == null) {
-				System.out.println("Sorry, unable to find " + filename);
-				return;
-			}
-
-			// load a properties file from class path, inside static method
-			prop.load(input);
-
-			prop.setProperty("key.serializer", StringSerializer.class.getName());
-			prop.setProperty("value.serializer", StringSerializer.class.getName());
-		} 
+		Properties prop = KafkaProperties.getProducerProperties(args);
 		
-		catch (IOException ex) {
-			ex.printStackTrace();
-		} 
-		
-		finally {
-			if (input != null) {
-				try {
-					input.close();
-				} 
-				catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+		if (prop == null) {
+			System.out.println("Sorry, unable to read properties.");
+			return;
 		}
 
 		Producer<String, String> producer = new org.apache.kafka.clients.producer.KafkaProducer<String, String>(prop);
 
 		for (int key = 0; key < 10; key++) {
-			ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("firsttopic",
+			ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>(prop.getProperty("topic"),
 					Integer.toString(key), "message with key " + Integer.toString(key));
 			producer.send(producerRecord);
 		}
 		producer.close();
-
-		// topic name
 
 	}
 }
--- TheKafkaSeries/kafka-examples/src/main/java/me/nikhil/kafkatester/KafkaProperties.java
+++ TheKafkaSeries/kafka-examples/src/main/java/me/nikhil/kafkatester/KafkaProperties.java
@@ -12,6 +12,7 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 public class KafkaProperties {
 	
@@ -25,7 +26,11 @@ public class KafkaProperties {
 	}
 	
 	private static void setProducerOptions(Options options) {
-		
+		options.addOption("bs", "bootstrap.servers", true, "A list of host/port pairs to use for establishing the initial connection to the Kafka cluster. The client will make use of all servers irrespective of which servers are specified here for bootstrapping—this list only impacts the initial hosts used to discover the full set of servers. This list should be in the form host1:port1,host2:port2,.... Since these servers are just used for the initial connection to discover the full cluster membership (which may change dynamically), this list need not contain the full set of servers (you may want more than one, though, in case a server is down).");
+		options.addOption("ack", "acks", true, "The number of acknowledgments the producer requires the leader to have received before considering a request complete. This controls the durability of records that are sent.");
+		options.addOption("ret", "retries", true, "Setting a value greater than zero will cause the client to resend any record whose send fails with a potentially transient error. Note that this retry is no different than if the client resent the record upon receiving the error. Allowing retries without setting max.in.flight.requests.per.connection to 1 will potentially change the ordering of records because if two batches are sent to a single partition, and the first fails and is retried but the second succeeds, then the records in the second batch may appear first.");
+		options.addOption("ling", "linger.ms", true, "The frequency in milliseconds to wait before sending messages.");
+		options.addOption("t", "topic", true, "The topic to which data is published.");
 	}
 
 	private static boolean loadPropertiesFromFile(String filename, Properties prop) {
@@ -60,6 +65,52 @@ public class KafkaProperties {
 			}
 		}
 		
+	}
+	
+	public static Properties getProducerProperties(String args[]) {
+		
+		/* 
+		 * The order of overriding:
+		 *  - Command line overrides
+		 *  - Properties file overrides
+		 *  - Handwritten in code
+		 */
+		
+		Properties prop = new Properties();
+		
+		/* set properties hand written in code */
+		prop.setProperty("key.serializer", StringSerializer.class.getName());
+		prop.setProperty("value.serializer", StringSerializer.class.getName());
+
+		/* load properties in properties file */
+		String filename = "producer.properties";
+		if (!loadPropertiesFromFile(filename, prop)) {
+			System.out.println("Couldn't load from properties file.");
+			return null;
+		}
+		
+		/* properties in command line */
+		Options options = new Options();
+		setProducerOptions(options);
+		
+		CommandLineParser parser = new DefaultParser();
+		try {
+			CommandLine cmd = parser.parse(options, args);
+			if (!cmd.hasOption("bootstrap.servers") || !cmd.hasOption("topic")) {
+				System.out.println("--bootstrap.servers and --topic are required arguments");
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.printHelp( "me.nikhil.kafkatester.KafkaProducerDemo", options );
+				return null;
+			}
+			for (Option option : cmd.getOptions()) {
+				System.out.println(option.getLongOpt() + " : " + option.getValue());
+				prop.setProperty(option.getLongOpt(), option.getValue());
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return prop;
 	}
 	
 	public static Properties getConsumerProperties(String args[]) {
